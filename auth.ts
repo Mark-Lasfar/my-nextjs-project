3c237eea608135e9491dd3ab5843186a86e3100c
lodesojlo <lodesojlo@gmail.com>
2025-04-28 02:38:15 +0300

    Update auth.ts
    update auth

--- auth.ts
+++ auth.ts
@@ -6,6 +6,7 @@ import { connectToDatabase } from './lib/db'
 import client from './lib/db/client'
 import User from './lib/db/models/user.model'
 
+
 import NextAuth, { type DefaultSession } from 'next-auth'
 import authConfig from './auth.config'
 
